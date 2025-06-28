b5f1bdd36beebb54c70a6365004c089a8a684522
Christian Seip <christian.seip@ravitar.com>
2025-06-28 13:08:02 +0200

    Bugfix: Mobile Menu Toggle

--- assets/controllers/menu_controller.js
+++ assets/controllers/menu_controller.js
@@ -1,9 +1,24 @@
-import { Controller } from '@hotwired/stimulus';
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-	static targets = ['panel'];
+	static targets = ['panel']
 
 	toggle() {
-		this.panelTarget.classList.toggle('hidden');
+		const panel = this.panelTarget
+		const isHidden = panel.classList.contains('hidden')
+
+		if (isHidden) {
+			panel.classList.remove('hidden')
+			requestAnimationFrame(() => {
+				panel.classList.remove('translate-y-[-10px]', 'opacity-0')
+				panel.classList.add('translate-y-0', 'opacity-100')
+			})
+		} else {
+			panel.classList.add('translate-y-[-10px]', 'opacity-0')
+			panel.classList.remove('translate-y-0', 'opacity-100')
+			setTimeout(() => {
+				panel.classList.add('hidden')
+			}, 300)
+		}
 	}
 }
